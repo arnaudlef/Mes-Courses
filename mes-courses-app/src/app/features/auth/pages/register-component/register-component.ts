dd169cc87808acc91a55707039a553f0b56c7710
arnaudlef <arn.lefrancois@hotmail.fr>
2026-05-12 19:09:43 +0200

    feat(register) : design page

--- mes-courses-app/src/app/features/auth/pages/register-component/register-component.ts
+++ mes-courses-app/src/app/features/auth/pages/register-component/register-component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register-component',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register-component.html',
   styleUrl: './register-component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  registerForm: FormGroup = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl('')
+  })
+
+  onSubmit() {
+
+  }
+}
